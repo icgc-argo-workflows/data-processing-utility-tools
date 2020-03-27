ce640159c722cde5eec43397459e8c6855704cae
Junjun Zhang <junjun.ca@gmail.com>
2020-03-27 14:30:48 -0400

    naming convention update

--- tools/payload-gen-dna-alignment/payload-gen-dna-alignment.py
+++ tools/payload-gen-dna-alignment/payload-gen-dna-alignment.py
@@ -74,13 +74,13 @@ def rename_file(f, payload, rg_count, sample_info):
     else:
         sys.exit('Error: unknown aligned seq extention: %s' % f)
 
-    new_name = "%s.%s.%s.%s.%s.%s.grch38.%s" % (
+    new_name = "%s.%s.%s.%s.%s.%s.%s" % (
         payload['studyId'],
         sample_info[0]['donor']['donorId'],
         sample_info[0]['sampleId'],
-        rg_count,
-        date.today().strftime("%Y%m%d"),
         library_strategy,
+        date.today().strftime("%Y%m%d"),
+        'aln',
         file_ext
     )
 
